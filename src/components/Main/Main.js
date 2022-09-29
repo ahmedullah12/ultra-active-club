8b8a4f5ee4b593a9667c359f9ad5c5ff78beee84
ahmedullah12 <gulamjoy321@gmail.com>
2022-09-29 16:59:36 +0600

    tasks added in page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Tasks from '../Tasks/Tasks';
 import './Main.css'
 
 const Main = () => {
+    const [tasks, setTasks] = useState([]);
+    
+    useEffect(() => {
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => setTasks(data))
+    }, [])
     return (
-        <div>
-            <h1 className='header'>Daily-Study-Tasks</h1>
+        <div className='activity-container'>
+            <div className='task-container'>
+                <h1 className='header'>Daily-Study-Tasks</h1>
+                <p className='task-header'>Select Today's Study Tasks</p>
+                <div className='tasks'>
+                    {
+                        tasks.map(task => <Tasks key={task.id} tasks = {task}></Tasks>)
+                    }
+                </div>
+            </div>
+
+            <div className='details-container'>
+                <h1>Ahmed</h1>
+            </div>
         </div>
+
     );
 };
 
---
+++ src/components/Tasks/Tasks.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Tasks.css'
+
+const Tasks = ({tasks}) => {
+    console.log(tasks)
+    const {image, name, time} = tasks;
+    return (
+        <div className='task'>
+            <img src={image} alt="" />
+            <h3>Subject Name: {name}</h3>
+            <p>Time Required: {time} minutes</p>
+            <button>Add to list</button>
+        </div>
+    );
+};
+
+export default Tasks;
