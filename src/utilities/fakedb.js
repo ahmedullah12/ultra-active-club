b66e7e9f12929959e8fdf695c805d9705ca750ed
ahmedullah12 <gulamjoy321@gmail.com>
2022-09-29 22:48:20 +0600

    got the stored break time from local storage

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -3,7 +3,14 @@ const addToDB = (time) => {
     localStorage.setItem('stored-time', JSON.stringify(storedBreakTime));
 }
 
+const getStoredTime = () => {
+    let storedBreakTime = {};
+    const storedTime = localStorage.getItem('stored-time');
+    if(storedTime){
+        storedBreakTime = JSON.parse(storedTime)
+    }
+    return storedBreakTime;
+}
 
 
-
-export {addToDB}
+export {addToDB, getStoredTime}
